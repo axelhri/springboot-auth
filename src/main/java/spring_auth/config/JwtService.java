0b4440c3a14355720b7a399341a25e031afb4918
Axelhri <axelhouairi@gmail.com>
2025-07-06 18:14:20 +0200

    build(pom.xml): jjwt dependencies installed

---
+++ src/main/java/spring_auth/config/JwtService.java
@@ -0,0 +1,10 @@
+package spring_auth.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
